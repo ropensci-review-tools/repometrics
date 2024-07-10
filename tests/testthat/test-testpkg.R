233517e6b24a90a03a33a4613bec92d4b8bfbcf0
mpadge <mark.padgham@email.com>
2024-07-10 14:56:32 +0200

    test 'githist' parameters

--- tests/testthat/test-testpkg.R
+++ tests/testthat/test-testpkg.R
@@ -10,3 +10,33 @@ test_that ("testpkg", {
     expect_identical (names (res), c ("desc_data", "loc", "stats"))
     expect_true (all (vapply (res, nrow, integer (1L)) > 1L))
 })
+
+test_that ("githist parameters", {
+    pkg <- system.file ("extdata", "testpkg.zip", package = "githist")
+    flist <- unzip (pkg, exdir = fs::path_temp ())
+    path <- fs::path_dir (flist [1])
+
+    res0 <- githist (path)
+    n0 <- vapply (res0, nrow, integer (1L))
+    fs::dir_delete (path)
+
+    flist <- unzip (pkg, exdir = fs::path_temp ())
+    res1 <- githist (path, n = 2L)
+    n1 <- vapply (res1, nrow, integer (1L))
+    fs::dir_delete (path)
+
+    flist <- unzip (pkg, exdir = fs::path_temp ())
+    res2 <- githist (path, n = 2L, step_size = 2L)
+    n2 <- vapply (res2, nrow, integer (1L))
+    fs::dir_delete (path)
+
+    expect_true (all (n0 > n1))
+    expect_true (all (n0 > n2))
+    expect_true (nrow (res0$desc_data) > 2L)
+    expect_equal (nrow (res1$desc_data), 2L)
+    expect_equal (nrow (res2$desc_data), 2L)
+
+    d1 <- abs (diff (res1$desc_data$date, units = "s"))
+    d2 <- abs (diff (res2$desc_data$date, units = "s"))
+    expect_true (mean (d1) < mean (d2))
+})
