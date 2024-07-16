af2e8fe9b2db7b8c8a59b9c20d582223f2c6d309
mpadge <mark.padgham@email.com>
2024-07-16 16:07:55 +0200

    more test-dashboard

--- tests/testthat/test-dashboard.R
+++ tests/testthat/test-dashboard.R
@@ -1,12 +1,12 @@
+pkg <- system.file ("extdata", "testpkg.zip", package = "githist")
+flist <- unzip (pkg, exdir = fs::path_temp ())
+path <- fs::path_dir (flist [1])
 
-test_that ("dashboard and input errors", {
+res0 <- githist (path)
 
-    pkg <- system.file ("extdata", "testpkg.zip", package = "githist")
-    flist <- unzip (pkg, exdir = fs::path_temp ())
-    path <- fs::path_dir (flist [1])
+test_that ("dashboard input errors", {
 
-    res0 <- res <- githist (path)
-
+    res <- res0
     res$desc_data <- NULL
     expect_error (
         ghist_dashboard (res, action = "build"),
@@ -37,4 +37,31 @@ test_that ("dashboard and input errors", {
         ghist_dashboard (res, action = "render"),
         "'results' contains empty tables."
     )
+})
+
+test_that ("dashboard build", {
+
+    res <- res0
+    ghist_dashboard (res, action = "render")
+
+    # Expect quarto docs to have been modified with package name:
+    pkg_name <- res0$desc_data$package [1]
+    path_tmp <- fs::path (fs::path_temp (), "quarto")
+    expect_true (fs::dir_exists (path_tmp))
+
+    f_index <- fs::path (path_tmp, "index.qmd")
+    index_qmd <- brio::read_lines (f_index)
+    i <- grep ("^title\\:", index_qmd)
+    expect_true (grepl ("\\{testpkg\\}", index_qmd [i]))
+
+    f_yaml <- fs::path (path_tmp, "_quarto.yml")
+    y <- brio::read_lines (f_yaml)
+    i <- grep ("^(\\s+?)title", y)
+    expect_true (grepl ("testpkg", y [i]))
+
+    # Expect site docs to have been built:
+    path_tmp_site <- fs::path (fs::path_temp (), "quarto", "_site")
+    expect_true (fs::dir_exists (path_tmp_site))
+    f_tmp_site <- fs::path (fs::path_temp (), "quarto", "_site", "index.html")
+    expect_true (fs::file_exists (f_tmp_site))
 })
