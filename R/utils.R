da77809f9ff914f0f41d020a2afbd3acc543b7b0
mpadge <mark.padgham@email.com>
2024-11-12 12:40:38 +0100

    doc utils fns

--- R/utils.R
+++ R/utils.R
@@ -1,3 +1,8 @@
+#' Return a tripartite vector of (mean, median, sum) of a given input vector
+#'
+#' @param x A numberic vector
+#' @return Named vector of three numeric values of (mean, median, sum)
+#' @noRd
 mn_med_sum <- function (x) {
     ret <- c (mean = 0, median = 0, sum = 0)
     if (length (x) > 0) {
@@ -6,6 +11,8 @@ mn_med_sum <- function (x) {
     return (ret)
 }
 
+#' Convert single values of length 0 or NULL to `NA_integer`.
+#' @noRd
 null2na_int <- function (x) {
     ifelse (length (x) == 0, NA_integer_, x)
 }
