4d388835ed30e8ad7836c2cc6bd94933d064e120
mpadge <mark.padgham@email.com>
2024-11-12 16:33:49 +0100

    add 'test-multicore.R' for #1

---
+++ tests/testthat/test-multicore.R
@@ -0,0 +1,17 @@
+test_that ("multicore", {
+    pkg <- system.file ("extdata", "testpkg.zip", package = "githist")
+    flist <- unzip (pkg, exdir = fs::path_temp ())
+    path <- fs::path_dir (flist [1])
+
+    res1 <- githist (path, num_cores = 1L)
+    # Becuase of hard reset of history, entire pkg dir needs to be deleted and
+    # re-created:
+    fs::dir_delete (path)
+
+    flist <- unzip (pkg, exdir = fs::path_temp ())
+    path <- fs::path_dir (flist [1])
+    res2 <- githist (path, num_cores = 2L)
+    fs::dir_delete (path)
+
+    expect_identical (res1, res2)
+})
