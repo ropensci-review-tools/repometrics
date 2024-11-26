c1771eb5c049d0b91a40e40172e4890cb26a4111
mpadge <mark.padgham@email.com>
2024-11-26 11:07:29 +0100

    rename 'cm_metrics_has_ci' for #23

--- R/cm-metrics-has-ci.R
+++ R/cm-metrics-has-ci.R
@@ -18,3 +18,22 @@ repo_has_ci_files <- function (path) {
 
     return (nms [which (has_it)])
 }
+
+cm_metric_has_ci <- function (path) {
+
+    is_test_env <- Sys.getenv ("REPOMETRICS_TESTS") == "true"
+    has_ci <- ifelse (is_test_env, FALSE, has_gh_ci_tests (path))
+
+    if (!has_ci) {
+        ci_files <- repo_has_ci_files (path)
+        has_ci <- length (ci_files) > 0L
+        if (has_ci) {
+            cli::cli_alert_info (paste0 (
+                "Unable to determine whether runs are ",
+                "recent for CI service [{ci_files}]."
+            ))
+        }
+    }
+
+    return (has_ci)
+}
--- tests/testthat/test-cm-metrics.R
+++ tests/testthat/test-cm-metrics.R
@@ -39,3 +39,27 @@ test_that ("chaoss has CI internal", {
 
     fs::dir_delete (path)
 })
+
+test_that ("cm metric has_ci", {
+
+    Sys.setenv ("REPOMETRICS_TESTS" = "true")
+
+    path <- generate_test_pkg ()
+
+    chk <- repo_has_ci_files (path)
+    expect_length (chk, 0L)
+
+    d <- fs::dir_create (fs::path (path, ".github", "workflows"))
+    f <- fs::path (d, "workflow.yaml")
+    writeLines ("a", f)
+
+    chk <- repo_has_ci_files (path)
+    expect_length (chk, 1L)
+    expect_equal (chk, "github")
+
+    # Test cli::cli_alert_warning output:
+    expect_snapshot (chk <- cm_metric_has_ci (path))
+    expect_true (chk)
+
+    fs::dir_delete (path)
+})
