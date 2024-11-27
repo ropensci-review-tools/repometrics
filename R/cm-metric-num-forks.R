0ebfa2f8321339b96a8fcd13cac4a2d5235468f3
mpadge <mark.padgham@email.com>
2024-11-27 13:31:10 +0100

    finish 'cm_metric_num_forks' fn for #11

--- R/cm-metric-num-forks.R
+++ R/cm-metric-num-forks.R
@@ -1,4 +1,9 @@
-cm_metric_num_forks <- function (path) {
-    repo_dat <- cm_data_repo_from_gh_api (path)
-    return (repo_dat$forks_count)
+cm_metric_num_forks <- function (path, end_date = Sys.Date ()) {
+
+    forks <- cm_data_repo_forks (path)
+
+    start_date <- end_date - get_repometrics_period ()
+    index <- which (forks$created >= start_date & forks$created <= end_date)
+
+    return (c (num_in_period = length (index), num_total = nrow (forks)))
 }
