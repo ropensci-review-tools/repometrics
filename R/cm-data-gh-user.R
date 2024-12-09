033814f7d70bfa50c9c1792a2a9735b1fcdc348a
mpadge <mark.padgham@email.com>
2024-12-09 13:07:58 +0100

    add user_contrib_collect_issue_cmts fn

--- R/cm-data-gh-user.R
+++ R/cm-data-gh-user.R
@@ -411,3 +411,126 @@ gh_user_contrib_collect_issues_internal <- function (login,
 }
 gh_user_contrib_collect_issues <-
     memoise::memoise (gh_user_contrib_collect_issues_internal)
+
+gh_user_contrib_collect_issue_cmts_qry <- function (login = "",
+                                                    nyears = 1,
+                                                    n_per_page = 100L,
+                                                    end_cursor = NULL) {
+
+    after_txt <- ""
+    if (!is.null (end_cursor)) {
+        after_txt <- paste0 (", after:\"", end_cursor, "\"")
+    }
+
+    q <- paste0 ("{
+        user(login:\"", login, "\") {
+            login
+            issueComments (first: ", n_per_page, after_txt, ", orderBy: { field: UPDATED_AT, direction: DESC } ) {
+                pageInfo {
+                    hasNextPage
+                    endCursor
+                }
+                totalCount
+                nodes {
+                    createdAt
+                    issue {
+                        number
+                        comments {
+                            totalCount
+                        }
+                        participants {
+                            totalCount
+                        }
+                        repository {
+                            nameWithOwner
+                        }
+                    }
+                }
+            }
+        }
+    }")
+
+    return (q)
+}
+
+gh_user_contrib_collect_issue_cmts_internal <- function (login,
+                                                         nyears = 1,
+                                                         n_per_page = 100L) {
+
+    is_test_env <- Sys.getenv ("REPOMETRICS_TESTS") == "true"
+    n_per_page <- n_per_page_in_tests (n_per_page)
+
+    issue_comment_counts <- 0L
+    end_cursor <- created_at <- org_repo <- issue_num <-
+        num_comments <- num_participants <- NULL
+    has_next_page <- TRUE
+
+    start_timestamp <- format (Sys.time () - 60 * 60 * 24 * 365 * nyears, "%Y-%m-%dT%H:%M:%S")
+    start_timestamp <- as.POSIXct (start_timestamp)
+
+    while (has_next_page) {
+
+        q <- gh_user_contrib_collect_issue_cmts_qry (
+            login = login,
+            n_per_page = n_per_page,
+            end_cursor = end_cursor
+        )
+        dat <- gh::gh_gql (query = q)
+
+        has_next_page <- dat$data$user$issueComments$pageInfo$hasNextPage
+        end_cursor <- dat$data$user$issueComments$pageInfo$endCursor
+
+        issue_comment_counts <-
+            issue_comment_counts + dat$data$user$issueComments$totalCount
+
+        nodes <- dat$data$user$issueComments$nodes
+
+        created_at <- c (
+            created_at,
+            vapply (nodes, function (i) i$createdAt, character (1L))
+        )
+        org_repo <- c (
+            org_repo,
+            vapply (
+                nodes,
+                function (i) i$issue$repository$nameWithOwner,
+                character (1L)
+            )
+        )
+        issue_num <- c (
+            issue_num,
+            vapply (
+                nodes, function (i) i$issue$number,
+                integer (1L)
+            )
+        )
+        num_comments <- c (
+            num_comments,
+            vapply (
+                nodes,
+                function (i) i$issue$comments$totalCount,
+                integer (1L)
+            )
+        )
+        num_participants <- c (
+            num_participants,
+            vapply (
+                nodes,
+                function (i) i$issue$participants$totalCount,
+                integer (1L)
+            )
+        )
+
+        if (tail (as.POSIXct (created_at), 1) < start_timestamp) {
+            has_next_page <- FALSE
+        }
+    }
+
+    data.frame (
+        org_repo = org_repo,
+        issue_num = issue_num,
+        created_at = created_at,
+        num_comments = num_comments,
+        num_participants = num_participants
+    )
+}
