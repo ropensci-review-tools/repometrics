d989bc0461773d367c7d368753dc22a4beee8ede
mpadge <mark.padgham@email.com>
2025-01-20 11:45:57 +0100

    add 'cm_metric_languages' for #16

---
+++ R/cm-metric-languages.R
@@ -0,0 +1,25 @@
+#' Extract CHAOSS metric "Programming Language Distribution"
+#'
+#' \url{https://chaoss.community/?p=3430}.
+#'
+#' @param path Local path to repository
+#' @noRd
+cm_metric_languages <- function (path) {
+
+    loc_stats <- utils::getFromNamespace ("loc_stats", "pkgstats")
+
+    s <- tryCatch (
+        loc_stats (path),
+        error = function (e) NULL
+    )
+    if (is.null (s)) {
+        return (NULL)
+    }
+
+    dplyr::group_by (s, language) |>
+        dplyr::summarise (nfiles = sum (nfiles), ncode = sum (ncode)) |>
+        dplyr::mutate (
+            nfiles_pc = nfiles / sum (nfiles),
+            ncode_pc = ncode / sum (ncode)
+        )
+}
