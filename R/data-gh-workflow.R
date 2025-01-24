49fe03bb7b4b42fcb1aeca0fb4f70f13d10d77fb
mpadge <mark.padgham@email.com>
2025-01-24 16:43:37 +0100

    add 'gh_workflow_test_coverage' for #14

--- R/data-gh-workflow.R
+++ R/data-gh-workflow.R
@@ -21,6 +21,7 @@ rm_data_gh_repo_workflow_internal <- function (path, n_per_page = 30L) { # nolin
 
     ids <- vapply (workflows, function (i) i$id, numeric (1L))
     names <- vapply (workflows, function (i) i$name, character (1L))
+    logs_url <- vapply (workflows, function (i) i$logs_url, character (1L))
     shas <- vapply (workflows, function (i) i$head_sha, character (1L))
     titles <- vapply (
         workflows,
@@ -44,6 +45,7 @@ rm_data_gh_repo_workflow_internal <- function (path, n_per_page = 30L) { # nolin
         name = names,
         id = ids,
         sha = shas,
+        logs_url = logs_url,
         title = titles,
         status = status,
         conclusion = conclusion,
@@ -51,3 +53,70 @@ rm_data_gh_repo_workflow_internal <- function (path, n_per_page = 30L) { # nolin
     )
 }
 rm_data_gh_repo_workflow <- memoise::memoise (rm_data_gh_repo_workflow_internal)
+
+
+gh_workflow_test_coverage_internal <- function (path) {
+
+    requireNamespace ("readr")
+
+    or <- org_repo_from_path (path)
+    wf_tests <- rm_data_gh_repo_workflow (path) |>
+        dplyr::filter (grepl ("test|coverage", name))
+
+    cov <- vapply (wf_tests$logs_url, function (i) {
+        coverage_from_one_log (i)
+    }, numeric (1L))
+
+    res <- data.frame (
+        id = wf_tests$id,
+        created = wf_tests$created,
+        coverage = cov,
+        row.names = NULL
+    ) |>
+        dplyr::filter (!is.na (coverage))
+
+    return (res)
+}
+gh_workflow_test_coverage <-
+    memoise::memoise (gh_workflow_test_coverage_internal)
+
+coverage_from_one_log <- function (log_url) {
+
+    cov <- NA_real_
+
+    req <- httr2::request (log_url) |>
+        add_gh_token_to_req ()
+
+    path <- fs::path (fs::path_temp (), "temp.zip")
+    resp <- tryCatch (
+        httr2::req_perform (req, path = path),
+        error = function (e) NULL
+    )
+    if (is.null (resp)) {
+        return (cov)
+    }
+
+    httr2::resp_check_status (resp)
+
+    if (fs::file_exists (path)) {
+        dirs_old <- fs::dir_ls (fs::path_temp (), type = "dir")
+        flist <- unzip (path, exdir = fs::path_temp ())
+        dirs_new <- fs::dir_ls (fs::path_temp (), type = "dir")
+        dirs_new <- dirs_new [which (!dirs_new %in% dirs_old)]
+
+        f_cov <- grep ("coverage\\.txt", flist, value = TRUE)
+        cov <- unlist (lapply (f_cov, function (f) {
+            txt <- readr::read_lines (f, progress = FALSE)
+            grep ("coverage\\s*:", txt, value = TRUE, ignore.case = TRUE)
+        }))
+        cov <- gsub ("^.*coverage\\s*:|%", "", cov, ignore.case = TRUE)
+        cov <- as.numeric (cov)
+        cov <- unique (cov [which (!is.na (cov))])
+
+        fs::file_delete (flist)
+        fs::file_delete (path)
+        fs::dir_delete (dirs_new)
+    }
+
+    return (max (cov))
+}
