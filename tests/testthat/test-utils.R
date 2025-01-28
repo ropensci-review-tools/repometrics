2db6fbb8fc5a35a54fee8dc2e470b7b93abb704e
mpadge <mark.padgham@email.com>
2025-01-28 15:09:46 +0100

    write actual tests for 'list_gh_org_repos' utils fn

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -73,4 +73,8 @@ test_that ("gh org repos", {
     repo_names <- httptest2::with_mock_dir ("utils_gh_org", {
         list_gh_org_repos (org)
     })
+
+    expect_type (repo_names, "character")
+    expect_length (repo_names, n_per_page_in_tests (30))
+    expect_false (any (grepl ("\\/", repo_names))) # bare names only
 })
