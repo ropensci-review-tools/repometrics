b2ce6c8db9a39327490cfeaaa618877c9d3f1f25
mpadge <mark.padgham@email.com>
2025-02-11 10:56:52 +0100

    add 'rm_org_emb_distances' fn for inter-pkg embedding distances

--- R/data-embeddings.R
+++ R/data-embeddings.R
@@ -1,4 +1,4 @@
-rm_org_data_embeddings <- function (org_paths, embeddings_data = NULL) {
+rm_org_data_embeddings <- function (org_paths) {
 
     requireNamespace ("pkgmatch")
 
@@ -7,6 +7,43 @@ rm_org_data_embeddings <- function (org_paths, embeddings_data = NULL) {
     embeddings <- pkgmatch::pkgmatch_embeddings_from_pkgs (pkg_names$paths)
 
     return (embeddings)
+}
+
+rm_org_emb_distances <- function (org_paths, embeddings_data = NULL, what = "code") {
+
+    what <- match.arg (what, c ("text_with_fns", "text_wo_fns", "code"))
+
+    if (is.null (embeddings_data)) {
+        embeddings_data <- rm_org_data_embeddings (org_paths)
+    } else {
+        check_embeddings_param (embeddings_data)
+    }
+
+    this_mat <- embeddings_data [[what]]
+    npkgs <- ncol (embeddings_data [[what]])
+    embeddings <- lapply (seq_len (npkgs), function (i) {
+        this_vec <- this_mat [, i]
+        cosine_similarity (this_vec, this_mat)
+    })
+    emb_matrix <- do.call (cbind, embeddings)
+    diag (emb_matrix) <- NA_real_
+
+    return (emb_matrix)
+}
+
+# Adapted from pkgmatch/R/similarity-metrics.R, but to just return vector of
+# similarities.
+cosine_similarity <- function (this_vec, this_mat) {
+
+    nrow <- length (this_vec)
+    ncol <- ncol (this_mat)
+    emb_mat <- matrix (this_vec, nrow = nrow, ncol = ncol)
+
+    cs_num <- colSums (emb_mat * this_mat)
+    cs_denom <- sqrt (colSums (emb_mat^2) * colSums (this_mat^2))
+    cs <- cs_num / cs_denom
+
+    return (cs)
 }
 
 check_embeddings_param <- function (embeddings_data) {
