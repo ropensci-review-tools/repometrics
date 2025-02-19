daf0f6aebf9f0330be50b4964ec158f545344ea3
mpadge <mark.padgham@email.com>
2025-02-19 15:07:08 +0100

    rm another org file for #105

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -65,16 +65,3 @@ test_that ("url from path", {
 
     fs::dir_delete (path)
 })
-
-test_that ("gh org repos", {
-
-    Sys.setenv ("REPOMETRICS_TESTS" = "true")
-    org <- "ropensci"
-    repo_names <- httptest2::with_mock_dir ("utils_gh_org", {
-        list_gh_org_repos (org)
-    })
-
-    expect_type (repo_names, "character")
-    expect_length (repo_names, n_per_page_in_tests (30))
-    expect_true (any (grepl ("\\/", repo_names))) # bare names only
-})
