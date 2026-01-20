e42fa4be8a98a0419f94edd9441066db63e1c977
mpadge <mark.padgham@email.com>
2026-01-20 09:43:19 +0100

    do not error on httr2 requests in gh-issues

--- R/data-api-gh-issues.R
+++ R/data-api-gh-issues.R
@@ -17,16 +17,19 @@ rm_data_issues_from_gh_api_internal <- function (path, n_per_page = 100) { # nol
 
         resp <- httr2::req_retry (req, max_tries = 5L) |>
             httr2::req_perform ()
-        httr2::resp_check_status (resp)
 
-        body <- c (body, httr2::resp_body_json (resp))
-
-        next_page <- gh_next_page (resp)
-        if (is_test_env) {
+        if (httr2::resp_is_error (resp)) {
             next_page <- NULL
-        }
+        } else {
+            body <- c (body, httr2::resp_body_json (resp))
 
-        req <- httr2::req_url_query (req0, page = next_page)
+            next_page <- gh_next_page (resp)
+            if (is_test_env) {
+                next_page <- NULL
+            }
+
+            req <- httr2::req_url_query (req0, page = next_page)
+        }
     }
 
     issues <- data.frame (
